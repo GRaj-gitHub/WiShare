ce8b23e09f8ce7650ecc5595d5b7e39c2840f578
GRaj-gitHub <rajgopika1105@gmail.com>
2021-09-22 13:46:21 +0100

    Initial commit

---
+++ WiShare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WiShare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
